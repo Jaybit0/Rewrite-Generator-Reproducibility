bddfd0b43968a935aca184df65db29e0e3f6e53f
Jaybit0 <jannik.lindemann3@gmail.com>
2025-01-16 16:54:52 +0100

    Update

--- microbenchmarks/experiments/eval/evalBar.py
+++ microbenchmarks/experiments/eval/evalBar.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.ticker as ticker
 
 test_name = 'multInv'
-sparsity = 1
+sparsity = 0.01
 save = True
 
 # Sort the data based on the desired order
@@ -13,6 +13,10 @@ var_order = [1000, 5000, 10000, 1000000, 5000000, 10000000]
 mode_order = ['Std', 'Gen']
 include = ['Std', 'Gen']
 format = "M"
+figsize = (6, 6)
+margin_left = 0.17
+fixed_maximum = 100
+legend_loc = 'upper left'
 
 
 
@@ -47,7 +51,7 @@ pivoted_data = data.pivot(index='VAR', columns='MODE', values='RESULT')
 bar_width = 0.2
 positions = np.arange(len(pivoted_data))
 
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=figsize)
 
 # Plot each MODE group
 for i, mode in enumerate(pivoted_data.columns):
@@ -59,14 +63,17 @@ plt.xlabel('Data Size', fontsize=18)
 plt.ylabel('Time [ms]', fontsize=18)
 plt.legend(title='MODE')
 plt.yscale('log')
-plt.ylim(0.1, None)
-plt.legend(fontsize=16, loc='lower right')  # Add a legend to distinguish the two lines
+plt.ylim(0.1, fixed_maximum)
+plt.legend(fontsize=16, loc=legend_loc)  # Add a legend to distinguish the two lines
 plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
 
 # Customize the x-axis ticks to show formatted labels
 formatted_labels = [formatting(var, None) for var in pivoted_data.index]
 plt.xticks(positions + bar_width * (len(pivoted_data.columns) / 2 - 0.5), formatted_labels, fontsize=16)
+
+fig = plt.gcf()  # Get the current figure
+fig.subplots_adjust(left=margin_left)  # Increase the left margin to make space for the y-axis label
 
 if save:
     plt.savefig("../figuresMB/" + test_name + "_" + str(sparsity) + ".pdf")
